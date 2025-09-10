cd9acc69b0be09db76222a9ddbac5d27d624f0e5
rscadata <rscadata@gmail.com>
2025-09-10 09:42:04 +0200

    Update FinalApp.py

--- FinalApp.py
+++ FinalApp.py
@@ -81,7 +81,12 @@ st.write("2 = Applied inconsistently / moderate level")
 st.write("3 = Applied consistently / strong impact")
 
 # tactical_fluidity = st.slider("Tactical Fluidity", 0, 3, 0)
-tactical_fluidity = st.select_slider("Tactical Fluidity", options=[0, 1, 2, 3], value=0)
+tactical_fluidity = st.radio(
+    "Tactical Fluidity",
+    options=[0, 1, 2, 3],
+    index=0,
+    horizontal=True
+)
 progressive_possession = st.slider("Progressive Possession", 0, 3, 0)
 off_ball_runs = st.slider("Off-Ball Runs", 0, 3, 0)
 counterpress = st.slider("Counterpress", 0, 3, 0)
@@ -127,6 +132,7 @@ if st.button("Submit evaluation"):
     ]
     sheet.insert_row(data, 2)
     st.success("✅ Evaluation successfully submitted!")
+
 
 
 
